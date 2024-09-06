111346f74a8cb1d0ee3f5b5dbd887118f06e92f7
Sudhir Mhamane <157618279+sudhirM45@users.noreply.github.com>
2024-09-06 05:05:14 +0000

    convserion operation done

---
+++ 01_Basics/03_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"// ye line js ke code ko newer vesion ke tarha treat karta hai 
+
+//alert(3+3) // ye hame error dega beacause we are using the node.js and not browser.
+
+/*
+#datatypes :
+
+    1.number -> int
+    2.String -> ""
+    3.boolean -> true/false
+    4.null -> is the dt in JS and 'stand alone value' and its 
+                empty value.
+    5.undefined -> we declear variable and don't assign      the value.
+    6. symbol -> when the unique term comes            
+*/
+
+console.log(typeof undefined) // o/p -> undefined
+console.log(typeof null) // o/p -> object
+
---
+++ 01_Basics/04_conversionOperation.js
@@ -0,0 +1,19 @@
+/*
+const: Declares a block-scoped variable that cannot be reassigned after its initial assignment. The variable's value is constant, but if it is an object or array, its contents can still be modified.
+
+let: Declares a block-scoped variable that can be reassigned. It's generally used for variables that need to be updated or changed.
+*/
+
+let score = 33
+
+ console.log(typeof score)
+ console.log(typeof(score));
+ // "33" -> get easly convert
+ //  "33abc" -> NaN(not a num)
+ //  "true" -> 1; "false" -> 0;
+
+ // in boolean 1-> true; 0-> false
+ // "" -> false : "sudhir" -> true
+
+ 
+
