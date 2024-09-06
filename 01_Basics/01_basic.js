84a4f69a5cd6c350beb0cbfd5b70dce7fab635ac
Sudhir Mhamane <157618279+sudhirM45@users.noreply.github.com>
2024-09-06 13:27:39 +0530

    Update 01_basic.js

--- 01_Basics/01_basic.js
+++ 01_Basics/01_basic.js
@@ -1,13 +1,17 @@
 // console.log("Sudhir")
 
 // 1.undefined in JS : 
-// in JavaScript means a variable has been declared but has not been assigned a value yet. It represents the "absence of a value."
+// in JavaScript means a variable has been declared but has not been assigned a value yet. 
+// It represents the "absence of a value."
 
 // 2.null in JS :
-//  in JavaScript is an intentional assignment representing "no value" or "empty value." It is explicitly set to indicate that a variable has no object or value.
+//  in JavaScript is an intentional assignment representing "no value" or "empty value." 
+//  It is explicitly set to indicate that a variable has no object or value.
 
 /* 3.Object in JS :
-   An Object in JavaScript is a collection of key-value pairs, where each key is a string (or symbol) and each value can be of any data type (like a number, string, array, or another object). Objects are used to store and organize data and functionality.
+   An Object in JavaScript is a collection of key-value pairs, where each key is a string 
+   (or symbol) and each value can be of any data type (like a number, string, array, or 
+    another object). Objects are used to store and organize data and functionality.
 
 For example:
 
@@ -24,7 +28,9 @@ Here, person is an object with properties name and age, and a method greet().
 */
 
 // 4.symbol in JS : 
-// A Symbol in JavaScript is a unique and immutable primitive value used to create unique property keys for objects. Symbols are often used to avoid property name collisions in objects, especially when building libraries or frameworks.
+// A Symbol in JavaScript is a unique and immutable primitive value used to create unique property
+// keys for objects. Symbols are often used to avoid property name collisions in objects, 
+//  especially when building libraries or frameworks.
 
 /* 
 we can create a symbol using the Symbol() function:
@@ -41,5 +47,6 @@ const sym2 = Symbol("example");
 
 console.log(sym1 === sym2); // false
 
-Symbols are typically used for defining private properties or methods on objects, ensuring they don't conflict with other property keys.
+Symbols are typically used for defining private properties or methods on objects, 
+ensuring they don't conflict with other property keys.
 */
