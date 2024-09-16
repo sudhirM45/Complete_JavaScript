1b504f7a16f29d5b87a9fdbac03fbe86e7ff95ce
Sudhir Mhamane <157618279+sudhirM45@users.noreply.github.com>
2024-09-16 07:24:10 +0000

    maths and nums done

---
+++ 01_Basics/07_maths_and_nums.js
@@ -0,0 +1,38 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+
+//console.log(balance.toString());
+
+const nums = 133.3333
+//console.log(nums.toPrecision(2));
+
+const oneNum = 10000000
+//console.log(oneNum.toLocaleString('en-IN'));
+//  ----------------xxx Maths for JS xxx----------------
+
+// console.log(Math);
+// console.log(Math.abs(-455));
+
+// console.log(Math.round(2.3));
+// console.log(Math.ceil(3.1)); //means it round the number to the upper value,ex-4
+// console.log(Math.floor(3.9));//means it round the number to the lower value,ex-3
+
+//console.log(Math.random()) // this method always gives the new random number in between the o and 1.
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
